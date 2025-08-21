c853d7f7f164b9725740a175c6bf366ed6c23c07
Davis Zarins <davis.zars@gmail.com>
2025-08-22 00:59:02 +0300

    Initial Commit

---
+++ ott-demo/ott_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ott_demoApp.swift
+//  ott-demo
+//
+//  Created by Davis Zarins on 22/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ott_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
